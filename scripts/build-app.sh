5eb7b718c0a246bef0fd92c3093ab51356976349
swairshah <swairshah@gmail.com>
2026-05-05 14:34:34 -0700

    chore: release v1.5.1

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -50,7 +50,7 @@ cat > "$APP_DIR/Contents/Info.plist" << 'EOF'
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.5.0</string>
+    <string>1.5.1</string>
     <key>CFBundleVersion</key>
     <string>1</string>
     <key>LSMinimumSystemVersion</key>
